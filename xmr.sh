a8e870bf445e7ca028c53f2db4ad6355fa05cc6c
dopir <86645092+dopir@users.noreply.github.com>
2021-06-29 12:18:50 +0700

    Create xmr.sh

---
+++ xmr.sh
@@ -0,0 +1,5 @@
+#xmr
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
+tar xf xmrig-6.12.2-linux-x64.tar.gz
+./xmrig -a rx/0 -o hk.monero.herominers.com:10190 -u 8AjTj2r3VVoJ8JakFinouUirLBLRaqc4FQcaCt3eg3hPJ3KtLTksrvzHLG7vDngh9rcB5RKrueeZTftKHgTkrfk96CaPLoB -p nana
